d1095d7f8de21eaacbf1f6d055e8209b79f605af
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-03-07 19:25:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function fibonacci(num) {
 // your code here\
-	if (num === 0) return 0;
-    if (num === 1) return 1;
-    return fibonacci(num - 1) + fibonacci(num - 2);
+	if (num === 1) return 0;
+  if (num === 2) return 1;
 
+  let a = 0, b = 1, temp;
+  
+  for (let i = 3; i <= num; i++) {
+    temp = a + b;
+    a = b;
+    b = temp;
+  }
+
+  return b;
 	
 }
 
