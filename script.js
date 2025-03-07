c088e11366eaae03f99f1bbe567492e894997c1c
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-03-07 18:58:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,21 +5,6 @@ function fibonacci(num) {
     return fibonacci(num - 1) + fibonacci(num - 2);
 
 	
-	   let num1 = 0;
-    let num2 = 1;
-    let sum;
-    if (num === 1) {
-        return num1;
-    } else if (num === 2) {
-        return num2;
-    } else {
-        for (let i = 3; i <= num; i++) {
-            sum = num1 + num2;
-            num1 = num2;
-            num2 = sum;
-        }
-        return num2;
-    }
 }
 
 module.exports = fibonacci;
